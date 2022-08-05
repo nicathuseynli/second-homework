5c25e1a44902108572f49596f8365034b61ef7cd
nicathuseynli <nijatkhh@code.edu.az>
2022-08-05 13:08:54 +0400

    first homework

--- Program.cs
+++ Program.cs
@@ -6,18 +6,42 @@ namespace first_homework_task_1_2_3
     {
         static void Main(string[] args)
         {
-            int i = 0;
-
-            Console.WriteLine("Even Numbers :");
-            for (i = 1; i <= 40; i++)
+             int evem_numbet = 40;
+            for (int i = 0; i <= 40; i++)
             {
                 if (i % 2 == 0)
                 {
-                    Console.Write(i + " ");
+                    Console.WriteLine(i + "");
                 }
             }
+            Console.ReadKey();
 
-           
+
+
+
+            int[] oddnumbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int sum = 0;
+            for (int i = 0; i < oddnumbers.Length; i++)
+            {
+                if (oddnumbers[i] % 2 == 1)
+                {
+                    sum += oddnumbers[i];
+                }
+            }
+            Console.WriteLine(sum);
+
+
+
+
+            double p, pin_num;
+            double PI = 3.14;
+            Console.WriteLine("Dairənin radiusunu daxil edin");
+            p = Convert.ToDouble(Console.ReadLine());
+            pin_num = 1 * PI * p;
+            Console.WriteLine("Dairə perimetri {0}", pin_num);
+            Console.Read();
+
+
         }
     }
 }
