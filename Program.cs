89083fcf2296c94bcfaea800d5db3eb03274ac92
nicathuseynli <nijatkhh@code.edu.az>
2022-08-05 12:50:50 +0400

    first homework

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace first_homework_task_1_2_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            Console.WriteLine("Even Numbers :");
+            for (i = 1; i <= 40; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+           
+        }
+    }
+}
